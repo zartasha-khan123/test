7878854e5cccabd9551f4f0c5b0f33bf2d71dd4c
zartasha-khan123 <zartashimran12345@gmail.com>
2024-03-26 02:34:38 +0500

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env node 
+
+import inquirer from "inquirer";
+
+//1) computer will generate a random number..
+
+//2) user input for guessing number
+
+//3) compare user input with computer generated number and show result.
+
+const randomNumber= Math.floor(Math.random() * 6 + 1 );
+
+//const message="This is number guessing game: ";
+
+const answers= await inquirer.prompt([
+    {
+        name:"zibsGuessedNumber",
+        type:"numbers",
+        message:"please guess a number between 1-6: ",
+    },
+]);
+
+if(answers.zibsGuessedNumber === randomNumber){
+    console.log("congragulations! you guessed right number: ")
+}
+else {
+    console.log("you guessed wrong number");
+}
